78db8cfc6e03b6634fc6758565b9dbb30d40d21a
hadware <hadwarez@gmail.com>
2019-12-19 03:01:10 +0100

    Completed (but not yet tested) the check_parser and assign_tasks CLI commands

--- seshat/cli_apps/assign_task.py
+++ seshat/cli_apps/assign_task.py
@@ -1,11 +1,11 @@
 from mongoengine import connect, DoesNotExist
 
-from seshat.models import Campaign
+from seshat.models import Campaign, Annotator, SingleAnnotatorTask, DoubleAnnotatorTask, BaseTask
 from .commons import argparser
 
 argparser.add_argument("campaign_slug", type=str, help="Campaign slug for which the task are assigned")
 argparser.add_argument("--files", type=str, nargs="*", help="List of files to assign")
-group = argparser.add_mutually_exclusive_group(required=True)
+group = argparser.add_mutually_exclusive_group("task_type", required=True)
 group.add_argument("--single", action="store_true")
 group.add_argument("--double", action="store_true")
 single_group = group.add_argument_group("single annotator task group")
@@ -14,16 +14,44 @@ double_group = group.add_argument_group("double annotators task group")
 single_group.add_argument("--target", type=str)
 single_group.add_argument("--reference", type=str)
 
+# TODO : add "assign as user" parameter, and better document how the files have to be listed
 
 def main():
     args = argparser.parse_args()
     connect(args.db)
     try:
-        campaigns: Campaign = Campaign.objects.get(slug=args.campaign_slug)
+        campaign: Campaign = Campaign.objects.get(slug=args.campaign_slug)
     except DoesNotExist:
-        ValueError("Cannot find campaign with slug %s" % args.campaign_slug)
+        raise ValueError("Cannot find campaign with slug %s" % args.campaign_slug)
 
-    # TODO
+    if args.task_type.single:
+        annotator = Annotator.objects.get(username=args["single_annot_assign"]["annotator"])
+        task_class = SingleAnnotatorTask
+        annotators = {"annotator": annotator}
+    else:
+        reference = Annotator.objects.get(username=args["double_annot_assign"]["reference"])
+        target = Annotator.objects.get(username=args["double_annot_assign"]["target"])
+        task_class = DoubleAnnotatorTask
+        annotators = {"reference": reference,
+                      "target": target}
+
+    for file in args.files:
+        new_task: BaseTask = task_class(**annotators)
+        template_doc = campaign.gen_template_tg(file)
+        template_doc.creators = []
+        template_doc.save()
+        new_task.template_tg = template_doc
+        new_task.campaign = campaign
+        new_task.data_file = file
+        new_task.assigner = campaign.creator
+        new_task.save()
+        template_doc.task = new_task
+        template_doc.save()
+        campaign.tasks.append(new_task)
+        for user in annotators.values():
+            user.assigned_tasks.append(new_task)
+    task_class.notify_assign(list(annotators.values()), campaign)
+    campaign.save()
 
 
 if __name__ == "__main__":
--- seshat/cli_apps/check_parser.py
+++ seshat/cli_apps/check_parser.py
@@ -1,10 +1,11 @@
+from inspect import signature
+
 from seshat.configs import set_up_db
 from .commons import argparser
 from seshat.parsers import list_parsers
 
 argparser.add_argument("-p", "--parser", type=str, help="Name of the parser, to check its validity")
 argparser.add_argument("-l", "--list", action="store_true", help="List all available parsers")
-
 
 def main():
     args = argparser.parse_args()
@@ -16,19 +17,23 @@ def main():
             for parser_name, parser in mod_parsers.items():
                 print(f"\t- {parser_name} ({parser_mod})")
     elif args.parser:
-        # TODO : check that parser has the required methods, run eventual tests
+
+        def search_parser(parsers_dict, parser_name):
+            for parser_mod, mod_parsers in parsers_dict.items():
+                for parser_name, parser_class in mod_parsers.items():
+                    if parser_name == args.parser_name:
+                        return parser_class, parser_mod
+            return None, None
+
         try:
             if "." in args.parser:
                 parser_mod, parser_name = args.parser.split(".")
                 queried_parser_class = parsers_dict[parser_mod][parser_name]
             else:
-                for parser_mod, mod_parsers in parsers_dict.items():
-                    for parser_name, parser in mod_parsers.items():
-                        if parser_name == args.parser_name:
-                            queried_parser_class = parser
-                            break
-                else:
+                queried_parser_class, parser_mod = search_parser(parsers_dict, args.parser)
+                if queried_parser_class is None:
                     raise KeyError()
+
         except KeyError:
             print(f"Couldn't find parser with name {args.parser}")
             return
@@ -37,14 +42,39 @@ def main():
         try:
             parser = queried_parser_class()
         except TypeError as err:
-            print(f"The check_parser method hasn't been properly overloaded: {str(err)}")
+            print(f"The 'check_annotation' method hasn't been properly overloaded: {str(err)}")
             return
         print("Checking methods...")
         # checking that the required methods are there, with the right signature
         parse_fn = getattr(queried_parser_class, "parse_annot", None)
         distance_fn = getattr(queried_parser_class, "distance", None)
 
-        # TODO
+        if parse_fn is None:
+            print("ERROR: the 'check_annotation' method couldn't be found on that parser")
+        else:
+            print("Detected 'check_annotation' method")
+            parse_sig = signature(parse_fn)
+            if len(parse_sig.parameters) != 1:
+                print(f"WARNING: the 'check_annotation' method should have only one parameters, "
+                      f"found {len(parse_sig.parameters)} parameters")
+            else:
+                print("Detected one parameter for 'check_annotation' (as it should be).")
+                print("'check_annotation' method seems to be valid")
+
+        if distance_fn is None:
+            print("WARNING: 'distance' method couldn't be found. This means no inter-rater agreement can be computed "
+                  "for Tiers checked with this parser")
+        else:
+            print("Detected a 'distance' method")
+            distance_sig = signature(distance_fn)
+            if len(distance_sig.parameters) != 2:
+                print(f"WARNING: the 'distance' method should have exactly 2 parameters, "
+                      f"found {len(distance_sig.parameters)} parameters")
+            else:
+                print("Detected one parameter for 'check_annotation' (as it should be).")
+                print("The 'distance' method seems to be valid")
+
+        # TODO : use default annotations to check
 
 
 if __name__ == "__main__":
