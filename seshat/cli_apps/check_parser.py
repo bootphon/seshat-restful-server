a33d56250fbae93b0b3996a13c95e783a4f688c4
hadware <hadwarez@gmail.com>
2020-01-11 02:59:51 +0100

    Fixed the parser checking code. Seems to be working fine

--- seshat/cli_apps/check_parser.py
+++ seshat/cli_apps/check_parser.py
@@ -1,4 +1,5 @@
 from inspect import signature
+from itertools import chain
 
 from seshat.configs import set_up_db
 from .commons import argparser
@@ -12,7 +13,8 @@ def main():
     set_up_db(args.config)
     parsers_dict = list_parsers()
     if args.list:
-        print("Found %i parsers:" % len(parsers_dict))
+        parsers_count = len(list(chain.from_iterable(parsers_dict.values())))
+        print(f"Found {parsers_count:d} parsers in {len(parsers_dict):d} modules:")
         for parser_mod, mod_parsers in parsers_dict.items():
             for parser_name, parser in mod_parsers.items():
                 print(f"\t- {parser_name} ({parser_mod})")
@@ -20,8 +22,8 @@ def main():
 
         def search_parser(parsers_dict, parser_name):
             for parser_mod, mod_parsers in parsers_dict.items():
-                for parser_name, parser_class in mod_parsers.items():
-                    if parser_name == args.parser_name:
+                for found_parser_name, parser_class in mod_parsers.items():
+                    if parser_name == found_parser_name:
                         return parser_class, parser_mod
             return None, None
 
@@ -46,7 +48,7 @@ def main():
             return
         print("Checking methods...")
         # checking that the required methods are there, with the right signature
-        parse_fn = getattr(queried_parser_class, "parse_annot", None)
+        parse_fn = getattr(queried_parser_class, "check_annotation", None)
         distance_fn = getattr(queried_parser_class, "distance", None)
 
         if parse_fn is None:
@@ -54,11 +56,11 @@ def main():
         else:
             print("Detected 'check_annotation' method")
             parse_sig = signature(parse_fn)
-            if len(parse_sig.parameters) != 1:
+            if len(parse_sig.parameters) != 2:
                 print(f"WARNING: the 'check_annotation' method should have only one parameters, "
                       f"found {len(parse_sig.parameters)} parameters")
             else:
-                print("Detected one parameter for 'check_annotation' (as it should be).")
+                print("Detected 1 parameter for 'check_annotation' (as it should be).")
                 print("'check_annotation' method seems to be valid")
 
         if distance_fn is None:
@@ -67,11 +69,11 @@ def main():
         else:
             print("Detected a 'distance' method")
             distance_sig = signature(distance_fn)
-            if len(distance_sig.parameters) != 2:
+            if len(distance_sig.parameters) != 3:
                 print(f"WARNING: the 'distance' method should have exactly 2 parameters, "
                       f"found {len(distance_sig.parameters)} parameters")
             else:
-                print("Detected one parameter for 'check_annotation' (as it should be).")
+                print("Detected 2 parameters for 'distance' (as it should be).")
                 print("The 'distance' method seems to be valid")
 
         # TODO : use default annotations to check
